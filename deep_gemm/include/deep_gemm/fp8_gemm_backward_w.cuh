8fbd1cc1bd30689822f4880e526435e1213bd30c
josephydu <josephyou@tencent.com>
2025-03-19 15:11:09 +0800

    tmp

--- deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
+++ deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
@@ -171,7 +171,6 @@ fp8_gemm_bw_kernel(__nv_bfloat16* gmem_d, float* scales_b, int* grouped_layout,
                         tma_copy<kNumTMAMulticast>(&tensor_map_scales_a, reinterpret_cast<uint64_t*>(&full_barrier),
                                                    smem_scales_a[s], m_block_idx * BLOCK_M,
                                                    scheduler.get_global_idx(SHAPE_K_SCALES, 1, k_idx / BLOCK_K));
-
                         // Issue TMA B without broadcasting
                         tma_copy(&tensor_map_b, reinterpret_cast<uint64_t*>(&full_barrier),
                                  smem_b[s], k_idx, scheduler.get_global_idx<false>(SHAPE_N, BLOCK_N, n_block_idx, m_block_idx));
