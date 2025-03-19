b40be68df3b0ae9d2b9150036c98a80fb1b1eb85
josephydu <josephyou@tencent.com>
2025-03-19 11:20:32 +0800

    tmp

--- deep_gemm/jit_kernels/m_grouped_gemm_dw.py
+++ deep_gemm/jit_kernels/m_grouped_gemm_dw.py
@@ -18,7 +18,7 @@ constexpr auto kNumStages = {NUM_STAGES};
 constexpr auto kNumTMAMulticast = {NUM_TMA_MULTICAST};
 
 // Make a templated GEMM
-using GemmType = GemmBW<N, K, BLOCK_M, BLOCK_N, 128, 1, kNumStages, kNumTMAMulticast, GemmType::GroupedContiguous>;
+using GemmType = GemmBW<N, K, BLOCK_M, BLOCK_N, 128, {NUM_GROUPS}, kNumStages, kNumTMAMulticast, GemmType::{GEMM_TYPE}>;
 
 // Launch kernel
 auto tma_a_desc = GemmType::make_2d_tma_a_desc(lhs, m);
