35df4fc7d8c41595495879e01989175ef57aa86f
josephydu <josephyou@tencent.com>
2025-03-19 11:27:46 +0800

    tmp

--- deep_gemm/jit_kernels/m_grouped_gemm_dw.py
+++ deep_gemm/jit_kernels/m_grouped_gemm_dw.py
@@ -56,12 +56,10 @@ def m_grouped_gemm_dw_fp8_fp8_bf16_nt_contiguous(lhs: Tuple[torch.Tensor, torch.
             which means that the i-th row of the LHS matrix will be multiplied with `rhs[m_indices[i]]`.
             Values of `m_indices` in every-m-alignment-block must also be the same.
     """
-    print("hello")
     lhs, lhs_scales = lhs
     rhs, rhs_scales = rhs
     m, k = lhs.shape
     num_groups, n, k_ = rhs.shape
-    print(num_groups)
     m_, n_ = out.shape
     m__ = m_indices.numel()
 
