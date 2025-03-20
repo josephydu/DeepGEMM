810c160aed181292a6a68e7e2c49a984cd82859b
josephydu <josephyou@tencent.com>
2025-03-20 16:25:06 +0800

    var len test

--- tests/test_core.py
+++ tests/test_core.py
@@ -95,6 +95,7 @@ def construct_dw_varlen_grouped(num_groups, m_list, k, n, is_masked):
     
     seq_len = torch.Tensor([0] + m_list)
     cu_seq_len = torch.cumsum(seq_len, dim=0).to(torch.int32).to('cuda')
+    print(cu_seq_len)
     for i in range(num_groups):
         x_fp8[0][i], x_fp8[1][i] = per_token_cast_to_fp8(x[cu_seq_len[i]:cu_seq_len[i + 1]])
         y_fp8[0][i], y_fp8[1][i] = per_token_cast_to_fp8(y[i])  # NOTE: per-token
