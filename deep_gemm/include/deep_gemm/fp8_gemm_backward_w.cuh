229cab90733d48124ba9729741c2e5ae62cc23e1
josephydu <josephyou@tencent.com>
2025-03-20 11:17:43 +0800

    tmp

--- deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
+++ deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
@@ -176,14 +176,8 @@ fp8_gemm_bw_kernel(__nv_bfloat16* gmem_d, float* scales_b, int* grouped_layout,
                         tma_copy(&tensor_map_b, reinterpret_cast<uint64_t*>(&full_barrier),
                                  smem_b[s], k_idx, scheduler.get_global_idx<false>(SHAPE_N, BLOCK_N, n_block_idx, m_block_idx));
                         // Only support normal gemm now. @kavioyu
-                        // tma_copy(&tensor_map_scales_b, reinterpret_cast<uint64_t*>(&full_barrier),
-                        //          smem_scales_b[s], n_block_idx * BLOCK_N, scheduler.get_global_idx<false>(SHAPE_N, 1, k_idx / BLOCK_K, m_block_idx));
-
-                        constexpr uint32_t group_size = 2048; // 4096 / 2
-                        const uint32_t group_offset = group_idx * group_size;
                         tma_copy(&tensor_map_scales_b, reinterpret_cast<uint64_t*>(&full_barrier),
-                                 smem_scales_b[s], n_block_idx * BLOCK_N, 
-                                 scheduler.get_global_idx<false>(group_size, 1, k_idx / BLOCK_K, 0) + group_offset);
+                                 smem_scales_b[s], n_block_idx * BLOCK_N, scheduler.get_global_idx<false>(0, 1, k_idx / BLOCK_K, m_block_idx));
                         full_barrier.arrive_and_expect_tx(SMEM_A_SIZE_PER_STAGE + SMEM_B_SIZE_PER_STAGE + SMEM_SCALES_A_SIZE_PER_STAGE + SMEM_SCALES_B_SIZE_PER_STAGE);
                     }
 
