c0d123404dda2d870fc578ae7b57df34d2cdb755
josephydu <josephyou@tencent.com>
2025-03-31 15:57:35 +0800

    construct data

--- tests/test_core.py
+++ tests/test_core.py
@@ -168,6 +168,7 @@ def construct_dw_varlen_xy_grouped(num_groups, groups_list, k, is_masked):
     for i, group in enumerate(groups_list):
         x_part = x[start_idx:start_idx + group]
         y_part = y[start_idx:start_idx + group]
+        print('x_part.shape:', x_part.shape, 'y_part.shape:', y_part.shape)
         gemm = x_part @ y_part.t()
         ref_out[start_idx:start_idx + group, start_idx:start_idx + group] = gemm
         start_idx += group
