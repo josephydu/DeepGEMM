3cf455ece4f8519a89c33bfedc0eb4c6215e641b
josephydu <josephyou@tencent.com>
2025-03-31 17:05:09 +0800

    construct data

--- tests/test_core.py
+++ tests/test_core.py
@@ -170,7 +170,7 @@ def construct_dw_varlen_xy_grouped(num_groups, groups_list, k, is_masked):
         x_part = x[start_idx:start_idx + group]
         y_part = y[start_idx:start_idx + group]
         gemm = x_part @ y_part.t()
-        ref_out[start_idx:start_idx + group, start_idx:start_idx + group] = gemm
+        ref_out[start_idx:start_idx + group] = gemm
         start_idx += group
         
     assert sum(groups_list) % 4 == 0, f'TMA alignment error: {groups_list}'
