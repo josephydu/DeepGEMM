209a3bb01ea61b0026592067711f02af4d1a0607
josephydu <josephyou@tencent.com>
2025-03-31 16:23:21 +0800

    construct data

--- deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
+++ deep_gemm/include/deep_gemm/fp8_gemm_backward_w.cuh
@@ -382,9 +382,9 @@ public:
     }
 
     template <typename T>
-    static CUtensorMap make_2d_tma_b_desc(T* global_address) {
-        return make_2d_tma_desc(global_address, Layout::ColMajor,
-                                SHAPE_K, SHAPE_N * (kGemmType != GemmType::Normal ? kNumGroups : 1), BLOCK_K, BLOCK_N);
+    static CUtensorMap make_2d_tma_b_desc(T* global_address, uint32_t shape_m) {
+        return make_2d_tma_desc(global_address, Layout::RowMajor,
+                                shape_m * (kGemmType == GemmType::GroupedMasked ? kNumGroups : 1), SHAPE_K, BLOCK_M, BLOCK_K);
     }
 
     template <typename T>
