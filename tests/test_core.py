4084266f75283fd9c85fb54d371cadbe82b757d2
josephydu <josephyou@tencent.com>
2025-03-31 15:45:52 +0800

    construct data

--- tests/test_core.py
+++ tests/test_core.py
@@ -156,6 +156,7 @@ def construct_dw_varlen_grouped(num_groups, m_list, k, n, is_masked):
 
 
 def construct_dw_varlen_xy_grouped(num_groups, groups_list, k, is_masked):
+    print(groups_list)
     x = torch.cat([torch.randn((m, k), device='cuda', dtype=torch.bfloat16) for m in groups_list], dim=0)
     y = torch.cat([torch.randn((n, k), device='cuda', dtype=torch.bfloat16) for n in groups_list], dim=0)
     out = torch.empty((sum(groups_list), sum(groups_list)), device='cuda', dtype=torch.bfloat16)
