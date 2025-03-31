4a62e0cb6f6880fdb6312d72b893080835884142
josephydu <josephyou@tencent.com>
2025-03-31 16:51:19 +0800

    construct data

--- deep_gemm/include/deep_gemm/scheduler.cuh
+++ deep_gemm/include/deep_gemm/scheduler.cuh
@@ -62,6 +62,7 @@ struct Scheduler {
         if constexpr (kGemmType == GemmType::Normal) {
             return block_idx * block_size;
         } else if (kGemmType == GemmType::GroupedContiguous) {
+            
             auto offset = kIgnoreGroupedForGroupedContiguous ? 0 : __ldg(grouped_layout + m_block_idx * BLOCK_M);
             return offset * shape_dim + block_idx * block_size;
         } else if (kGemmType == GemmType::GroupedMasked) {
